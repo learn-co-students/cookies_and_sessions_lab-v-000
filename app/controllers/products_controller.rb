3085aa82f9358b207207f3c8813727d7880d4098
c1505 <coreym247@gmail.com>
2016-03-01 01:35:42 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    @products = session[:cart]
+    @cart = session[:cart]
   end
 
   def add
-    
+    cart
+    session[:cart] << params[:item]
+    redirect_to products_path
   end
 
 end
