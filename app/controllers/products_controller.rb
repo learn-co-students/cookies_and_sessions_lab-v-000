25d1a39e8004c182a0f11137281d9ab6e59012e6
azheng249 <andy.zheng249@gmail.com>
2016-03-20 14:02:21 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+  def index
+    @cart = cart
+  end
+
+  def add
+    @cart = cart
+    @cart << params[:product]
+
+    redirect_to products_index_path
+  end
+end
