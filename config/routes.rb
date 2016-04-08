33d41037d521ee09058b1cd7fde750ff073a92fe
shinyrachel <rachel.kory@gmail.com>
2016-04-08 12:58:26 -0400

    all specs except cart.rb passing - capybara errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  root 'application#index'
+
+  resources :products, only: [:index]
+  post 'products/add' => 'products#add'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
