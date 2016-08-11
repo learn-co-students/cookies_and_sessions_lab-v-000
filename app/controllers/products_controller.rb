e890a70bfbd1bef1aa66f670cf08f59411ae514b
Jason <jasonkwong11@gmail.com>
2016-08-11 08:59:43 -0400

    done

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+  def index
+    @cart = cart
+  end
+
+  def add
+    cart << params[:product]
+    redirect_to '/'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  post '/add', to: 'products#add'
+  root :to => 'products#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
