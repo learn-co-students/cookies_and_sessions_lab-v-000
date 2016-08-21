68100fb4035b26c458a4a1e3fb980b7d3ab32ad8
DEREJE GEMECHU <dereje88@gmail.com>
2016-08-21 23:33:24 +0000

    Done.

--- spec/features/cart_spec.rb
+++ spec/features/cart_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "homepage", :type => :view do
   it "adds items to the cart when they're submitted on its form" do
     visit '/'
-    fill_in 'product', with: 'kumquats'
+    fill_in 'products_product', with: 'kumquats'
     click_button 'add to cart'
     expect(page.body).to include 'kumquats'
   end
