3c61aff7985423e968e010970d0e72fc041775c7
Joseph Lim <joseph.d.lim@gmail.com>
2016-08-22 09:13:06 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
   def cart
-    if !session[:cart] || session[:cart] == nil
-      session[:cart] = []
-    end
-    session[:cart]
+    @cart = (session[:cart] ||= [])
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,12 @@ class ProductsController < ApplicationController
 
 
   def index
+    cart
   end
 
   def add
     cart << params[:product]
-    redirect_to :index
+    render :index
   end
 
 end
