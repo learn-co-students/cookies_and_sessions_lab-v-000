eece98a4751cddfe1b2b6447503a7450304a030a
CodeByLine <yleventhal@ymail.com>
2016-09-14 11:36:14 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,16 @@
+class ProductsController < ApplicationController
+
+
+  def index
+
+    @cart=cart
+  end
+
+  def add
+
+    cart << (params[:product])
+    render :index
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: 'products#index'
+
+  post '/', to: 'products#add'
+  # resources :cart # do
+    # resources :products
+  # end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
