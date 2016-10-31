2b44058ee644a40f879b29ee0a2e0449b6d58759
bshap93 <bshap93@gmail.com>
2016-10-30 20:07:58 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all # session[:cart].map{|item_name| Product.create(name: item_name)}
-    @product = Product.new
+    cart
   end
 
   def add
-    # raise params.inspect
-    @product = Product.create(product_params)
-    cart = session[:cart] || []
-    cart << @product.id
-    session[:cart] = cart
-    redirect_to "/"
+    cart << params[:product]
+    render :index
   end
-
-  def create
-    @cart = session[:cart]
-    @cart.products.create(product_params)
-    redirect_to products_path
-  end
-
-  private
-
-    def product_params
-      params.require(:product).permit(:name)
-    end
 end
