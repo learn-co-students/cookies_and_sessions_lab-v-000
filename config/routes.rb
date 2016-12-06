8f1f98771553bfc15cdd900cdc4c1a162e69c6c5
Joel <joelbitar1986@gmail.com>
2016-12-06 18:17:56 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/add'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#index'
+  post 'products/add', as: 'add_product'
+  get '/products' => 'products#index'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
