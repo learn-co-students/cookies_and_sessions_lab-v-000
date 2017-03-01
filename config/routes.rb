fda007ac67f1a8e5060d062e421fd49548165605
Jon Fougner <jon.fougner@gmail.com>
2017-03-01 01:26:44 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/' => 'application#index'
+  post 'add_to_cart' => 'application#add_to_cart'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
