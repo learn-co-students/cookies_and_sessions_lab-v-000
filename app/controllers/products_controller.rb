2d1901bb571287b22e5d51646b2e907bf188a02a
kkashuda <kkashuda@gmail.com>
2017-05-08 15:28:47 -0500

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+require 'pry'
+class ProductsController < ApplicationController
+  def index
+    cart
+  end 
+  
+  def add
+    product = params[:product]
+    cart << product
+    render :index
+  end
+end 
