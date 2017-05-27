e2c011d108b366fdd8e86dfc810cabd6940fa37b
Christina L. Cole <christinalcole@gmail.com>
2017-05-27 00:12:20 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'products#index'
+
+  post '/add_to_cart', to: 'products#add_to_cart'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
