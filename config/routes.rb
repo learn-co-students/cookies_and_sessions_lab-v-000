9e13265ce08780421cfedb70466cf59366a5cc7b
vicision <duncan.j.mclachlan@gmail.com>
2017-07-13 01:07:54 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  # get 'Products/index'
+  #
+  # get 'Products/add'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -12,7 +18,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  post '/', to: 'products#add'
 
   # Example resource route with options:
   #   resources :products do
