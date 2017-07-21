31301506100ead6dbcecae283a9a2530e54ecdfd
Saad <Dropheaven@live.com>
2017-07-21 12:14:02 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,8 +5,7 @@ class ProductsController < ApplicationController
   end
 
   def add
-    item = params[:product]
-    cart << item
+    cart << params[:product]
     redirect_to products_path
   end
 end
