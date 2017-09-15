32a6f8284f43986f04c3d02f0536cd5093cbcf35
skmcloughlin <skmcloughlin@gmail.com>
2017-09-15 15:02:10 -0400

    Done.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_cookies_and_sessions_lab_session'
+#ails.application.config.session_store :cookie_store, key: 'cart'
