d3f32ee0f42f72e3c0dcbda640b8527eb7421c65
Terry Blue <terryblue@comcast.net>
2017-09-23 00:57:58 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
-    @cart = cart
+    session[:cart] = cart
   end
 
   def add
--- spec/views/products_index_spec.rb
+++ spec/views/products_index_spec.rb
@@ -2,20 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "products/index", :type => :view do
   before do
-    binding.pry
     controller.singleton_class.class_eval do
       protected
         def cart
           session[:cart]
         end
-        binding.pry
         helper_method :cart
     end
   end
 
   it "shows everything in the cart" do
     session[:cart] = ['apples', 'bananas', 'pears']
-    binding.pry
     render
     expect(rendered).to include 'apples'
     expect(rendered).to include 'bananas'
