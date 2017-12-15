28c6667e185087195470902bbaaa3a52aad16924
github username <github email address>
2017-12-15 16:59:39 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    cart
   end
 
   def add
     @product = params[:product]
     cart << @product
-    redirect_to products_path
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: "application#homepage"
+  root "products#index"
   post '/products/add', to: "products#add"
-  resources :products, only: :index
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
