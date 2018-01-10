ffaf571bc3f616d5ceb749d528c625b48000b7b6
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-10 13:15:44 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,10 @@ class ProductsController < ApplicationController
 
   def index
     @cart_items = cart
-    # binding.pry
   end
 
   def add
-    session[:cart] = cart
+    # session[:cart] = cart
     session[:cart] << params[:product]
     redirect_to '/'
   end
--- scraps.rb
+++ scraps.rb
@@ -3,3 +3,9 @@
     <p><%= item %></p>
   <% end %>
 <% end %> -->
+
+
+<% @cart_items.each do |item| %>
+  <%= binding.pry %>
+  <p><%= item %></p>
+<% end %>
