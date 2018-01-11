2cdfaf7dda77f3e8ce925bd96bd1fbc887ae3e91
github username <github email address>
2018-01-11 11:17:41 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+
+  root 'products#index' #root is index action in products controlers
+  post '/' => 'products#add'  #(post to root?)add action , posts controller
+
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
