3b87ff4c747354e1022c0066fa1dead30156bdfa
Eric McKinney <mckinney99@gmail.com>
2018-02-02 17:24:55 +0000

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+class ProductsController < ApplicationController
+
+  def index
+    @cart = cart
+  end
+
+  def add
+    @cart = cart
+    @cart << params[:product]
+    redirect_to "/"
+  end
+
+end
