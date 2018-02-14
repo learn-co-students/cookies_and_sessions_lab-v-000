f10949f1ac82a8304fc42c1b5de7eaa1ead5bb48
Chris Gullickson <cgullickson5@gmail.com>
2018-02-14 00:31:31 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get '/' => 'products#index'
+  post '/products/add' => 'products#add'
 end
