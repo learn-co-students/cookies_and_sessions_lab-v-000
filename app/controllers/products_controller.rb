ce7e201a326ec4a0890d9593dcaf045de8a872d9
Scott Wakeman <github email address>
2018-03-13 16:36:31 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,13 +4,8 @@ class ProductsController < ApplicationController
   end
 
   def add
-    # Get the item from the path
-    @item = Item.find(params[:id])
-
     # Load the cart from the session, or create a new empty cart.
-    cart << @item.id
-
-    # Save the Cart in the Session
-    session[:cart] = cart
+    cart << params[:product]
+      render :add 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
-  get 'products/add'
 
+  get 'products/add' => 'products#add'
+  post 'products/add' => 'products#add'
+  
   root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
