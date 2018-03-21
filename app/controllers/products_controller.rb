14eacac75f2ef835e68f0d6d3c20ffc79140099d
NolanHughes <Nhughes987@gmail.com>
2018-03-21 16:53:06 -0500

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+
+  def index
+    # @cart = cart
+  end
+
+  def add
+    item = params[:product]
+
+    session[:cart] << item
+
+    redirect_to products_path
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def cart
+    session[:cart] ||= []
+  end
 end
