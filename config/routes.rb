c8e2fee3bdf7b6681a3d2303b0221f2ab346aef1
jacobsilver2 <jacobsilver2@mac.com>
2018-04-06 04:49:09 -0400

    third

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get '/products', to: 'products#index', as: 'index'
-  post '/products', to: 'products#add', as: 'add'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'products#index'
+  post '/', to: 'products#add'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
