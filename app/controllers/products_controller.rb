62548c8c4e8ce17bdfcdddef40a96fafe1405144
ingridwong0715 <ingrid830715@gmail.com>
2018-04-11 14:36:45 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,9 @@
 class ProductsController < ApplicationController
   def index
-    @products = cart
   end
 
   def add
     cart << params[:product]
-    binding.pry
-    redirect_to '/'
+    render :index
   end
 end
