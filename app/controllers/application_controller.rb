21dfdfbd078b2e82624c99bcb642133c2e47f26c
Sean Oughton <seanoughton@gmail.com>
2018-05-12 00:09:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def cart
-    if session[:cart]
-      @cart = session[:cart] 
-    else
+    if !session[:cart]
       session[:cart] = []
-      @cart = session[:cart] 
     end
-    @cart
-    
+    @cart = session[:cart]
+
     # this method should return an array of the items stored in the cart (utilizing the Rails session method).
   end
 end
