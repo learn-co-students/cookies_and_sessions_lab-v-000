36534056fa47461f6112457d4bec1eb6b525d5c2
Eden <emesfun@outlook.com>
2018-05-12 10:53:03 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+
+    def index 
+        if cart.nil?
+            @cart = []
+        else
+            @cart = cart
+        end   
+    end 
+
+    def add
+        add_to_cart
+        redirect_to root_path
+    end
+end
