ed7bb0ab3b58fe95d0d858b95ea02abb19e629dd
Sean Oughton <seanoughton@gmail.com>
2018-05-14 16:45:04 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,7 @@ class ProductsController < ApplicationController
   def create
     @product = params[:product]
     cart << @product
+    @cart = cart
     render "index"
-    #need to add this item to the cart
-    #need to send this to the view to show a list of products
-    #need to display
   end
 end
