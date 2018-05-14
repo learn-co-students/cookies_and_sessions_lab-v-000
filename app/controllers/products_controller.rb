559dc289eafd295753a4392945f4a80fb8ac2139
Sean Oughton <seanoughton@gmail.com>
2018-05-14 17:14:58 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,6 @@ class ProductsController < ApplicationController
   def create
     @product = params[:product]
     cart(@product)
-    #byebug
-    #redirect_to index
-    #@cart = cart
     redirect_to '/products'
   end
 end
