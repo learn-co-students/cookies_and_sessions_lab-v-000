8332e3cffb5099c575228d8b022e46e65d127307
Sean Oughton <seanoughton@gmail.com>
2018-05-14 15:59:54 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,12 +5,10 @@ class ProductsController < ApplicationController
   end
 
   def new
+
   end
 
   def create
     byebug
-    #need to either manually create an add route or figure out how to alias one
-    @cart = params[:product]
-    redirect_to procuct_path
   end
 end
