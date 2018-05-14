4bef61291ab331a1c5a2d2f2034fa2d342dff84e
Sean Oughton <seanoughton@gmail.com>
2018-05-14 16:55:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def cart(something)
+  def cart(product = "")
     if !session[:cart]
       session[:cart] = []
+    end
+    if !product.empty?
+      session[:cart] << product
     end
     session[:cart]
     # this method should return an array of the items stored in the cart (utilizing the Rails session method).
