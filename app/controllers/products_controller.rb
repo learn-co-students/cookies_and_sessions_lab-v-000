80e072dda7f34f8a85bd14060f6ab28488c5f6ab
Raiet Intraub <raietsaeed@gmail.com>
2018-06-04 21:40:55 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,17 @@
 class ProductsController < ApplicationController
 
   def index
+    @cart = current_cart
+    # puts "current_cart = #{current_cart} || #{@cart}"
+    # @cart.each do |item|
+    #   puts "item = #{item}"
+    # end
   end
 
   def add
+    # puts "Params = #{params}"
+    current_cart << params[:product]
+    redirect_to '/'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'products/add', to: 'products#add'
-  root to: 'products#index'
+  get '/', to: 'products#index'
+  post '/products/add', to: 'products#add'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
