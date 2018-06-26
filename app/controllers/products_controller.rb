0635b4c8bc299afeafc51f7ad2419431a803f25a
github username <github email address>
2018-06-26 17:08:33 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,9 @@
 class ProductsController < ApplicationController
   def index
     @cart = cart
-    render :index
   end
 
   def add
-    binding.pry
     cart << params[:product]
     redirect_to root_path
   end
