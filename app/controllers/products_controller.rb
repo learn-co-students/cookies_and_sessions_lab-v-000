fb7750393bd4f181cb7de96bfca4f5c51de738be
Nicholas Powers <njpowers7915@gmail.com>
2018-07-11 16:30:43 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,9 @@
 class ProductsController < ApplicationController
-
   def index
-    cart = session[:cart]
-    @products = cart
   end
 
   def add
-    @product = Product.find_or_create_by(name: params[:product][:product])
-    cart << @product
-    session[:cart] = cart
-    redirect_to '/'
+    cart << params[:product]
+    render :index
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'products#index'
-  get '/', to: 'products#index'
   post '/', to: 'products#add'
 
 
