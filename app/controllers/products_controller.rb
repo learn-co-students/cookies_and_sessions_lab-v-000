73df7c5385537e419f43c9766289864f2d2d74a5
Kaitlin Kelly <kk504408@gmail.com>
2018-07-15 12:29:31 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,7 @@ require 'pry'
 class ProductsController < ApplicationController
 
   def index
-    cart = session[:cart] || []
-    @products = []
-    cart.each do |product|
-      @products << product
-    end
 
-    binding.pry
   end
 
   def create
