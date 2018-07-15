cee0f2a5b09115bb3d8d185500d3c3cd8ad2961f
Kaitlin Kelly <kk504408@gmail.com>
2018-07-15 12:34:59 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,15 +2,12 @@ require 'pry'
 class ProductsController < ApplicationController
 
   def index
-
   end
 
   def create
-    cart = session[:cart] || []
     cart << params[:product]
-    session[:cart] = cart
 
-    redirect_to products_path
+    render :index
   end
 
 end
