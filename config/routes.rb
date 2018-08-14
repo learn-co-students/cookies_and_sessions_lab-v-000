3157371e97f31857362e8f5a1e13e9aa2344168f
Suchitra Naik <susanaik@gmail.com>
2018-08-14 18:32:16 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'products#homepage'
+  post '/' => 'products#add'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # resources :products
 
   # Example resource route with options:
   #   resources :products do
