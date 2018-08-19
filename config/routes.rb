2e642d0d0aa404c6f085a8d3138f1706c5a0aaed
Jayon Kim <jpkim921@gmail.com>
2018-08-19 20:04:36 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'products#index'
+  post '/' => 'products#add'
+
 end
