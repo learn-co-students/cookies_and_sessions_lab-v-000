86187fd355d0e108a4573081dd8b3fb684adad8c
Katrina Brinson <kbrinso@gmail.com>
2018-08-23 01:02:18 +0000

    one failing

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def add
+    # Get the item from the path
+    # @item = Item.find(params[:id])
+
+    # Load the cart from the session, or create a new empty cart.
+    # cart << @item.id
+  end
+end
