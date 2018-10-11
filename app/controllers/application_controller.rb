f913f12c9c7e715bfaf6fb3629840e6f49a888e2
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-11 01:34:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  helper_method :cart
 
   def cart
-  
+
   #  @item = Item.find(params[:id])
     if session[:cart] == nil
       cart =  []
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,13 @@
 class ProductsController < ApplicationController
 
   def index
-    
+
   end
 
   def add
 
     item = params[:product]
     cart << item
+    render :index
   end
 end
