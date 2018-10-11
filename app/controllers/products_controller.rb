6cdffda43803b1f0c1cc5b222c5b0325bcf41529
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-11 01:43:22 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
-    binding.pry
-    @cart
+
   end
 
   def add
-
     item = params[:product]
     cart << item
     render :index
