efd46e371df03d9eb63925ab72d99531b4449828
Everett <everett.bowline@gmail.com>
2018-10-12 23:31:37 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
 
   def index
+    @cart = cart
   end
 
   def add
-    @cart << params[:product]
+    cart << params[:product]
+    @cart = cart
     render :index
   end
 
