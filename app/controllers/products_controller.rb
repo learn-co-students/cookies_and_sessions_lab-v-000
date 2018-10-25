43f2a8d95bf2445847d1db12dcce554435e2e349
vickicello <vicki.cello@gmail.com>
2018-10-25 12:46:51 -0700

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+
+  def index
+    @cart = cart
+  end
+
+  def add
+    @item = params[:product]
+    cart << @item
+    redirect_to '/'
+  end
+
+
+end
