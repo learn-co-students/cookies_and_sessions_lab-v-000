1061c0a07b717516f7652a361ba5dd8daa222a26
Anu Khambete <anukhambete@gmail.com>
2018-10-25 19:48:24 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,19 @@
+require 'pry'
 class ProductsController < ApplicationController
 
   def index
+    #binding.pry
+    @cart = cart
   end
-  
+
+  def new
+    @cart = cart
+  end
+
+  def create
+    #binding.pry
+    redirect_to products_path
+  end
+
+
 end
