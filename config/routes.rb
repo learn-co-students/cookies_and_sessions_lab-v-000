7da5c9984bf118817dbaf0746224bc5ff4df2dcf
MargaritaKhodzka <mkhodzka@gmail.com>
2019-03-06 21:31:55 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  resources :products, only: [:index]
+  post '/products', to: 'products#add'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
