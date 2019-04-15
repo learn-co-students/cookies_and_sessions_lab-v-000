671d09d6c0de6561382ebdf2fdf77f375bc90d68
github username <github email address>
2019-04-15 17:08:56 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+   get '/' => 'application#index'
+  post '/' => 'products#add'
+  resources :products, only: [:index]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
