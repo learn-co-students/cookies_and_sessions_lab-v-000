f186397517161fab24db07b953493e4763a49935
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-14 19:41:27 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'product#index'
+  post '/' => 'product#add'
 end
