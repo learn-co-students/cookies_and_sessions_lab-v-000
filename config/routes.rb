782d3e33e0ad67b5b451bb8e282ae2bdfc8905e2
Meg Gutshall <meghan.gutshall@gmail.com>
2019-05-20 21:00:44 -0400

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/products/index', to: 'products#index'
+  post '/products/add', to: 'application#cart'
+
+  root 'products#index'
 end
