3418e809a04eb4f9f4d4cd65384a5c209f4825fa
Meg Gutshall <meghan.gutshall@gmail.com>
2019-05-20 21:26:39 -0400

    Fix routes to reflect root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/products/index', to: 'products#index'
-  post '/products/add', to: 'application#cart'
+  get '/', to: 'products#index'
+  post '/', to: 'products#add'
 
   root 'products#index'
 end
