953d11b3d864e015bfec0f2632f2511d7293271a
Srija S <srijakumar7@gmail.com>
2019-06-09 19:52:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :cart
 
 
   def cart #this method should return an array of the items stored in the cart (utilizing the Rails session method).
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
-  get 'products/add'
-
-  get 'product/index'
-
-  get 'product/add'
+root 'products#index'
+post '/' => 'products#add'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
