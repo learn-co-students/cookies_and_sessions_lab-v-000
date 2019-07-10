5ff0c0e79479e8f03f1eb628dfe95bf1e8c97828
leothebeagle <a.kosber@gmail.com>
2019-07-10 15:44:09 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/add'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# get '/patients/:id', to: 'patients#show', as: 'patient'
+  root 'products#index'
+
+  post '/cart', to: 'products#add'
 end
