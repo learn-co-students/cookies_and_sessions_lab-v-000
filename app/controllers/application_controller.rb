0e210c72e43d9356c2ea0aa9330398bd12029f7c
Jessica Romans <RomansHomeComputer@Jessicas-iMac.local>
2019-07-31 15:09:06 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :cart
 
+  # The method helper_method is to explicitly share some methods defined 
+  # in the controller to make them available for the view. This is used 
+  # for any method that you need to access from both controllers and helpers/views 
+  #   (standard helper methods are not available in controllers). e.g. common use case:
+
   def cart
     session[:cart] ||= []
   end 
