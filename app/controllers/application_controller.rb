eb9b7b14b736cea0ecc3dfcc7358ef4821754871
Sushi Chanrai <sushi@pathworks.co>
2019-08-21 19:46:54 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+helper_method :cart
+
+  def cart
+    session[:cart] ||= []
+    #if session [:cart] is nil then execute code/assignment on right
+    #i.e. assign an empty array to it
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+
+  #root 'products#index'
+  post '/' => 'products#add'
+  get '/', to: 'products#index'
+  #get '/patients/:id', to: 'patients#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
