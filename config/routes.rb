f06e26edf732ef462aae597004415fe17d9fe3f0
github username <github email address>
2020-01-14 15:12:20 +0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'products#index'
+  root 'products#index' #The root route only routes GET requests to the action
   post '/' => 'products#add'
 end
